13324d8d589d19f4e22fd9e9e8c545ca41801dca
尚辉 <shanghui@belink.com>
2020-07-09 14:44:37 +0800

    创建第一个springboot博客blog测试项目

---
+++ src/main/java/com/xiong/blog/BlogApplication.java
@@ -0,0 +1,40 @@
+package com.xiong.blog;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+
+@SpringBootApplication
+@Configuration
+@EnableEncryptableProperties
+@MapperScan("com.xiong.blog.dao") //扫描的mapper
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+
+    @Component
+    public class ErrorConfig implements ErrorPageRegistrar {
+
+        @Override
+        public void registerErrorPages(ErrorPageRegistry registry) {
+            ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/error");
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error");
+            registry.addErrorPages(error400Page,error401Page,error404Page,error500Page);
+        }
+    }
+
+}
